2f9de9509c8f7a07ff37835043787518bf25ec72
Andre <Andre>
2016-02-11 14:52:19 +0700

    Commit Ketiga

--- kmi-intranetdev/KMI-INTRANET/Controllers/RiwayatCutiController.cs
+++ kmi-intranetdev/KMI-INTRANET/Controllers/RiwayatCutiController.cs
@@ -28,7 +28,7 @@ namespace KMI_INTRANET.Controllers
         OracleConnection cmd;
         OracleDataAdapter da;
         DataSet ds = new DataSet();
-  
+
         public ActionResult Filter(SearchModel model)
         {
             string str = Request.Params["btn1"];
@@ -117,38 +117,103 @@ namespace KMI_INTRANET.Controllers
         }
         public ActionResult ExportToExcel()
         {
-            var products = new System.Data.DataTable("teste");
-            products.Columns.Add("col1", typeof(int));
-            products.Columns.Add("col2", typeof(string));
+            String filename;
 
-            products.Rows.Add(1, "product 1");
-            products.Rows.Add(2, "product 2");
-            products.Rows.Add(3, "product 3");
-            products.Rows.Add(4, "product 4");
-            products.Rows.Add(5, "product 5");
-            products.Rows.Add(6, "product 6");
-            products.Rows.Add(7, "product 7");
+            DataSet objDataSet = new DataSet();
+            filename = "~/UploadedExcel/RiwayatCuti.xls";
+            System.IO.FileInfo file = new System.IO.FileInfo(Server.MapPath(filename));
+            System.IO.StreamWriter fs = new System.IO.StreamWriter(Server.MapPath(filename), false);
 
+            fs.WriteLine("<?xml version=\"1.0\"?>");
+            fs.WriteLine("<?mso-application progid=\"Excel.Sheet\"?>");
+            fs.WriteLine("<Workbook xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\"");
+            fs.WriteLine("xmlns:o=\"urn:schemas-microsoft-com:office:office\"");
+            fs.WriteLine("xmlns:x=\"urn:schemas-microsoft-com:office:excel\"");
+            fs.WriteLine("xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\">");
 
-            var grid = new GridView();
-            grid.DataSource = products;
-            grid.DataBind();
+            fs.WriteLine(" <Styles>");
+            fs.WriteLine(" <Style ss:ID=\"HEAD\">");
+            fs.WriteLine(" <Font ss:Size=\"12\" ss:Bold=\"1\"/>");
+            fs.WriteLine(" <Alignment ss:Horizontal=\"Center\" ss:Vertical=\"Center\"/>");
+            fs.WriteLine(" </Style>");
+            fs.WriteLine(" <Style ss:ID=\"HeadField\">");
+            fs.WriteLine(" <Font ss:Size=\"12\" ss:Bold=\"1\"/>");
+            fs.WriteLine(" <Alignment ss:Horizontal=\"Center\" ss:Vertical=\"Center\"/>");
+            fs.WriteLine(" <Borders>");
+            fs.WriteLine(" <Border ss:Position=\"Bottom\" ss:LineStyle=\"Continuous\" ss:Weight=\"2\"/>");;
+            fs.WriteLine(" <Border ss:Position=\"Top\" ss:LineStyle=\"Continuous\" ss:Weight=\"2\"/>");
+            fs.WriteLine(" <Border ss:Position=\"Left\" ss:LineStyle=\"Continuous\" ss:Weight=\"2\"/>");
+            fs.WriteLine(" <Border ss:Position=\"Right\" ss:LineStyle=\"Continuous\" ss:Weight=\"2\"/>");
+            fs.WriteLine(" </Borders>");
+            fs.WriteLine(" <Interior ss:Color=\"#FFFF00\" ss:Pattern=\"Solid\"/>");
+            fs.WriteLine(" </Style>");
+       
 
-            Response.ClearContent();
-            Response.Buffer = true;
-            Response.AddHeader("content-disposition", "attachment; filename=MyExcelFile.xls");
-            Response.ContentType = "application/ms-excel";
+            fs.WriteLine(" </Styles>");
 
-            Response.Charset = "";
-            StringWriter sw = new StringWriter();
-            HtmlTextWriter htw = new HtmlTextWriter(sw);
+            fs.WriteLine("<Worksheet ss:Name=\"Riwayat Cuti\">");
+            fs.WriteLine(" <Table>");
 
-            grid.RenderControl(htw);
+            fs.WriteLine(" <Row ss:Index='1'>");
+            fs.WriteLine(" <Cell ss:Index='1' ss:MergeAcross='9'  ss:StyleID='HEAD'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">KAWASAKI MOTOR INDONESIA</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" </Row>");
+            fs.WriteLine(" <Row ss:Index='2'>");
+            fs.WriteLine(" <Cell ss:Index='1' ss:MergeAcross='9'  ss:StyleID='HEAD'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">CKD PART FOR KAWASAKI MOTORCYCLES</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" </Row>");
+            fs.WriteLine(" <Row ss:Index='3'>");
+            fs.WriteLine(" <Cell ss:Index='1' ss:MergeAcross='9'  ss:StyleID='HEAD'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">Nah Loh</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" </Row>");
 
-            Response.Output.Write(sw.ToString());
-            Response.Flush();
+            fs.WriteLine(" <Row ss:Index='5'>");
+            fs.WriteLine(" <Cell ss:Index='1' ss:StyleID='HeadField'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">NO</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" <Cell ss:Index='2' ss:StyleID='HeadField'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">PART NO</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" <Cell ss:Index='3' ss:StyleID='HeadField'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">DESCRIPTION</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" <Cell ss:Index='4' ss:StyleID='HeadField'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">QTY(PCS)</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" <Cell ss:Index='5' ss:StyleID='HeadField'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">H.S.NO</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" <Cell ss:Index='6' ss:StyleID='HeadField'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">H.S.10</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" <Cell ss:Index='7' ss:StyleID='HeadField'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">RVC</Data>");
+            fs.WriteLine(" </Cell>");
+
+            fs.WriteLine(" <Cell ss:Index='8' ss:StyleID='HeadField'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">GROSS WEIGHT</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" <Cell ss:Index='9' ss:StyleID='HeadField'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">NET WEIGHT</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" <Cell ss:Index='10' ss:StyleID='HeadField'>");
+            fs.WriteLine(" <Data ss:Type=\"String\">FOB</Data>");
+            fs.WriteLine(" </Cell>");
+            fs.WriteLine(" </Row>");
+
+            fs.WriteLine(" </Table>");
+            fs.WriteLine("</Worksheet>");
+            fs.WriteLine("</Workbook>");
+            fs.Close();
+
+            Response.Clear();
+            Response.ContentType = "application/vnd.excel";
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + file.Name + "");
+            Response.TransmitFile(Server.MapPath("~/UploadedExcel/RiwayatCuti.xls"));
             Response.End();
-
             return View();
         }
     }
